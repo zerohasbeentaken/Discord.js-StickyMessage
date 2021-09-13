af5f3de3fa8ee08ea51961813e3fcc69f2f70809
Zero <87039022+zerohasbeentaken@users.noreply.github.com>
2021-09-13 05:54:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-  res.send("Hello Hell!")
+  res.send("Hello World")
 })
 
 app.listen(3000, () => {
@@ -54,46 +54,5 @@ client.on("messageCreate", async message => {
     channel = ""
     message.channel.send("Successfully removed the message!")
   }
-  if(message.content.toLowerCase().startsWith("?h")) {
-    setTimeout(async () => {
-      let webhooks = await message.channel.fetchWebhooks()
-      setInterval(() => {
-        let messages = [
-          "Oh",
-          "ok",
-          "Hello",
-          "You are not the clown. You are the entire circus.",
-          "Also, according to YouTube statistics, only about 10 percent of people who watch my videos are actually subscribed. So if you end up liking this video, consider subscribing. It's free, and you can always change your mind later.",
-          "why are we a bot? oh right for the video",
-          "hello youtube viewer and yes i broke the 4th wall and theres nothing you can do about it",
-          "subscribe to imagine gaming play (can you let my family go now)",
-          "HELP US WE ARE BEING HELD HOSTAGE!",
-          "No"
-        ]
-        webhooks.random().send(messages[Math.floor(Math.random() * messages.length)])
-      }, Math.floor(Math.random() * 5) + 3 * 1000)
-    }, 30000)
-  }
-})
-async function simpsGenerator(channel) {
-  let i = 0
-  let names = [
-    {name: "amogus", avatar: "https://pbs.twimg.com/profile_images/1364920241265012743/Y__158zv.png"},
-    {
-      name: "egdewroth", avatar: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/ace-attorney-miles.jpg"
-    },
-    {
-    name: "pablo", avatar: "https://pfps.gg/assets/pfps/7791-pablo.png",
-    }, 
-    {
-      name: "hank", avatar: "https://i.kym-cdn.com/entries/icons/original/000/037/614/sussy_baka.png"
-    }
-    ]
-while(i !== 5) {
-  let h = names[Math.floor(Math.random() * names.length)]
-  await channel.createWebhook(h.name, {
-    avatar: h.avatar
-  })
-}
-}
+  
 client.login(process.env.token)
